1402f1e40824a238a4379b9aed171915940cf671
itznur07 <nuruddinalways99@gmail.com>
2025-02-12 12:43:08 +0600

    fixed issues

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include all files in src
+    './node_modules/stepzen-ui/**/*.{js,jsx,ts,tsx}', // Include library files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
